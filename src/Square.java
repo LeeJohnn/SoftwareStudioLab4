f3292083fcf35b9e9cfe1a2270626d991834ce18
Lee Emma <Lee Emma>
2016-03-24 18:09:41 +0800

    dwsfc

--- src/Square.java
+++ src/Square.java
@@ -11,25 +11,30 @@ public class Square extends Shape {
 	}
 	
 	
+	
 	@Override
 	public Color getRandomColor() {		
 		// TODO Return random color produce from java.util.Random;
 		// Java 'Color' class takes 3 floats, from 0 to 1.
-		
+		Random rdm = new Random();
+		float r = rdm.nextFloat();
+		float g = rdm.nextFloat();
+		float b = rdm.nextFloat();
+		Color randomColor = new Color(r, g, b);
 		// TODO Return color produced by three rgb floats.
-		return new Color(r, g, b);
+		return randomColor;
 	}
 	
 	@Override
 	public int getShapeWidth(){
 		// TODO Return Square width
-		return 0;
+		return shapeWidth;
 	}
 	
 	@Override
 	public int getShapeHeight(){
 		// TODO Return Square height
-		return 0;
+		return shapeWidth;
 	}
 
 
